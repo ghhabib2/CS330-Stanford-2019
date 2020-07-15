ecce3899a1ba94688b25315cf432b9aeb43be223
ch-shin <ch.shin@snu.ac.kr>
2020-07-15 01:31:23 +0000

    Add a script for problem3

---
+++ hw1/problem3.sh
@@ -0,0 +1,4 @@
+python hw1.py --num_classes=2 --num_samples=1
+python hw1.py --num_classes=3 --num_samples=1
+python hw1.py --num_classes=4 --num_samples=1
+python hw1.py --num_classes=4 --num_samples=5
